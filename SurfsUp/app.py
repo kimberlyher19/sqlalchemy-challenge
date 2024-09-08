6a37543c0d1ed4f4b9ca1bbd7dbc0eed16ae1592
kimberlyher19 <kher9541@Yahoo.com>
2024-09-08 17:55:04 -0500

    updated code and fixed engine link

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -13,12 +13,12 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(engine)
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
 Measurement = Base.classes.measurement
@@ -33,7 +33,27 @@ session = Session(engine)
 app = Flask(__name__)
 
 
-
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Hawaii Climate App!<br/>"
+        f"<br/>"
+        f"Availble Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start/end<br/>"
+        f"<p>'Start' and 'End' date should be in the format MMDDYYYY.<p>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
